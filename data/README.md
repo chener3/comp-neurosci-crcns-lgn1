8ed1129e5ae14922ef2f5c6097992469922f8505
chener3 <cc9hd@virginia.edu>
2021-05-02 22:47:15 +0800

    renamed project and added read_neuron to io

--- data/README.md
+++ data/README.md
@@ -1,2 +1,14 @@
 
-Edit this file to describe how to retrieve the data set. Except for very small data files, it's not recommended to check data into version control.
+The description of the dataset:from CRCNS.org, crcns_lgn-1 They study the Mouse LGN Orientation Selectivity
+
+These files contain the spiking responses of LGN neurons in the mouse to drifting gratings. Each file contains the spiking responses and stimulus specifications for a single neuron.
+
+These files are in the Matlab format. Loading one in MatLab will bring a structure called mlgn into MatLab's memory. The structure has two elements:
+
+spktimes: MxNxT size array, where M is the stimulus number, N is the repeat number and T is time in milliseconds. Value of 1 in the array indicates a spike. Value of 0 indicates no spike. The order of the presentation of the stimulus repeats is not provided.
+
+stim: M size array, where M is the number of stimulus conditions. stim for this dataset indicates the contrast used, spatial frequency, temporal frequency, stimulus duration (in seconds), prestimulus duration, poststimulus duration, the original scanrate, orientation, and whether muscimol was present in V1.
+
+To retrieve this dataset, we are using loadmat() function of Python to load the data of first neuron(neuron1), to print the data, mlgn, spktimes and stim of first neuron.
+
+Next, we are going to try the glob function to traverse all the neurons' data.
--- src/io.py
+++ src/io.py
@@ -2,3 +2,47 @@
 # -*- mode: python -*-
 """Functions for file IO"""
 from __future__ import print_function, division, absolute_import
+#I choose another dataset from the CRCNS, which is lgn-1. 
+
+#These files contain the spiking responses of LGN neurons in the mouse to drifting gratings.
+#Each file contains the spiking responses and stimulus specifications for a single neuron.
+
+from scipy.io import loadmat
+
+neuron1 = loadmat("data/mouselgn/mlgnori_01.mat")   #load data, loading one in MatLab will bring a structure called mlgn into MatLab's memory
+print(neuron1.keys())                               #check the keys
+print(neuron1)                                      #check the data
+
+print("mlgn ->", neuron1["mlgn"])                  # You access elements of a dict by key using the familiar bracket syntax:
+print("spktimes of mlgn ->",neuron1["mlgn"]["spktimes"])  # the spktimes of mlgn, MxNxT size array, where M is the stimulus number, N is the repeat number and T is time in milliseconds
+print("stim of mlgn ->",neuron1["mlgn"]["stim"])          # the stim of mlgn, M size array, where M is the number of stimulus conditions
+from os import path
+import glob
+
+def load_spikes(fname):
+    """Load spikes from a file in flat ascii format"""
+    with open(fname, "rt") as fp:
+        return [np.fromstring(line, sep=" ") for line in fp]
+
+# The '*' character will match any file name, so this `glob` call will return a list
+# of all the files in `data/mouselgn/`
+for dirname in glob.glob("data/mouselgn/*"):
+    neuron = path.basename(dirname)
+    print(" neuron:", neuron)
+    for respfile in glob.glob(path.join(dirname, "*")):
+        stim = path.splitext(path.basename(respfile))[0]
+        print("  stim:", stim)
+        for i, trial in enumerate(load_spikes(respfile)):
+            print("   trial", i, ":", len(trial), "spikes")
+
+spike_data = {}
+for dirname in glob.glob("data/mouselgn/*"):
+    neuron = path.basename(dirname)
+    animal = neuron.split("_")[0]
+    stims = []
+    for respfile in glob.glob(path.join(dirname, "*")):
+        stim = path.splitext(path.basename(respfile))[0]
+        trials = load_spikes(respfile)
+        stims.append({"stimulus": stim, "response": trials})
+    ndata = {"animal": animal, "stimuli": stims}
+    spike_data[neuron] = ndata
